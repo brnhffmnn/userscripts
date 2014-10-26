3768ceabad6c61d8bf0c3aaf10cdf3faee6a7dc7
Brian Hoffmann <github@slowpoke.de>
2014-10-26 10:56:10 +0100

    add DB Kreditkarte - WISO MeinGeld

---
+++ db-kreditkarte-wiso-meingeld/README.md
@@ -0,0 +1,4 @@
+DB Kreditkarte -> WISO MeinGeld
+===========
+
+Verändert die Rechnungsansicht des BahnCard Kreditkartenbankings unter https://www.kreditkartenbanking.de/bahncard für die Web Copy Importfunktion in WISO MeinGeld.
---
+++ db-kreditkarte-wiso-meingeld/update.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name DB Kreditkarte WISO MeinGeld
+// @namespace http://slowpoke.de/
+// @description Konvertiert die Umsaetze der DB Kreditkarte in ein Format das in WISO MeinGeld mit Web-Copy importiert werden kann.
+// @version 1.5.3
+// @downloadURL https://github.com/panzerfahrer/userscripts/raw/master/db-kreditkarte-wiso-meingeld/DBKreditkarte_WISOMeinGeld.user.js
+// @updateURL https://github.com/panzerfahrer/userscripts/raw/db-kreditkarte-wiso-meingeld/update.user.js
+// @include https://www.kreditkartenbanking.de/bahncard/cos_std/dispatch.do*
+// @run-at document-end
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// ==/UserScript==
